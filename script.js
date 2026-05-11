368490537e3d1297371e842b5d78cb6ca30f7032
Jephtha Sheran Thevarasa <you@example.com>
2026-05-11 17:24:35 +0200

    KLAR

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-let hour = 00;
-let minute = 00;
-let second = 00;
-let count = 00;
 
-timer = true;
-stopWatch();
+function easteregg() {
+        document.getElementById("clock").style.color = "gren";
+        document.querySelector("body").style.color = "";
+}
 
-document.getElementById('hr').innerHTML = hrString;
-        document.getElementById('min').innerHTML = minString;
-        document.getElementById('sec').innerHTML = secString;
-        document.getElementById('count').innerHTML = countString;
-        setTimeout(stopWatch, 10);
+function darkmode() {
+
+        document.body.style.background = "url(pictures/spacelogo.jpg)";
+        document.body.style.backgroundAttachment = "cover"
+        document.body.style.backgroundAttachment = "fixed"
+        
+}
+
+function timer() {
+        var i = 0; // a counter which is displayed every 100ms
+
+        // create interval with callback every 100ms.
+        // `interval` holds interval ID, used to clear the interval (stop callback)
+        var interval = setInterval(function () {
+                clock.innerHTML = i++; // writes to html ever
+
+                //if i is greater than 1000
+                if (i > 100000) {
+                        clearInterval(interval)
+                        easteregg()
+                }
+        }, 100);
+}
+
+timer();
